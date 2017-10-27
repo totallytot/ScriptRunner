38de878f1bb0a669883b75104469fa0a23a06166
AbstarctElement <fcukthepop@gmail.com>
2017-10-27 11:13:45 +0300

    yo

---
+++ src/main/resources/SetPriority.groovy
@@ -0,0 +1,28 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.issue.fields.CustomField
+
+CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager()
+CustomField storyPointsField = customFieldManager.getCustomFieldObject(10307L)
+
+String sp = issue.getCustomFieldValue(storyPointsField)
+
+if (sp == null || sp.isEmpty()) sp = "0"
+
+double spD = Double.parseDouble(sp)
+
+switch (spD) {
+    case(0):
+    case(1):
+    case(2):
+    case(3):
+        issue.setPriorityId("5")
+        break
+    case(4):
+    case(5):
+    case(6):
+    case(7):
+    case(8):
+        issue.setPriorityId("4")
+        break
+}
