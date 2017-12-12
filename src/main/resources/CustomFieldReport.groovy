4ec3a32a1a3b6ff28d3054a7b6fcd10db975972d
AbstarctElement <fcukthepop@gmail.com>
2017-12-12 11:40:35 +0300

    yo

---
+++ src/main/resources/CustomFieldReport.groovy
@@ -0,0 +1,10 @@
+//Put to Administration -> Ad-dons -> Script Console.
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.fields.CustomField;
+List<String> cfs = new ArrayList<>();
+for (CustomField cf : ComponentAccessor.getCustomFieldManager().getCustomFieldObjects())
+{
+    cfs.add("ID: " + cf.getIdAsLong() + " NAME: " + cf.getName() + " TYPE: " + cf.getCustomFieldType().getName());
+}
+return cfs.each{};
