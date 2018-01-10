f063181ca4ab2f3d0a8b552ee56e9a0a45c89c4f
AbstarctElement <fcukthepop@gmail.com>
2018-01-10 10:38:20 +0300

    yo

--- src/main/resources/RenameActiveWorkflow.groovy
+++ src/main/resources/RenameActiveWorkflow.groovy
@@ -1,3 +1,8 @@
+/**
+ * After running the script, go to workflow scheme with affected workflow and updated scheme by adding any change (assign any issue type to any workflow in the scheme and revert the change => Publish).
+ * This will trigger changes for old issues, which use affected workflow. Otherwise, workflow buttons will disappear from issue view on all issues, that use affected workflow and had been created prior to workflow renaming.
+ */
+
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.bc.JiraServiceContextImpl;
 import com.atlassian.jira.bc.workflow.WorkflowService;
