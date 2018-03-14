5371bf784a8d85dbc1fa6f62069f0eb9394e0b49
AbstarctElement <fcukthepop@gmail.com>
2018-03-14 14:50:17 +0300

    yo

---
+++ src/main/resources/jira/DeleteInactiveWorkflows.groovy
@@ -0,0 +1,30 @@
+package jira
+
+//system default workflow should in some scheme
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.workflow.JiraWorkflow;
+import com.atlassian.jira.workflow.WorkflowManager;
+import com.atlassian.jira.workflow.WorkflowSchemeManager;
+
+WorkflowManager workflowManager = ComponentAccessor.getWorkflowManager();
+WorkflowSchemeManager workflowSchemeManager = ComponentAccessor.getWorkflowSchemeManager();
+StringBuilder report = new StringBuilder();
+
+Collection<JiraWorkflow> workflows = workflowManager.getWorkflows();
+workflows.removeAll( workflowManager.getActiveWorkflows());
+
+workflows.each {
+    if (workflowSchemeManager.getSchemesForWorkflow(it).size() == 0) {
+        report.append(it.getName());
+        report.append(" | ")
+        workflowManager.deleteWorkflow(it);
+    }
+}
+return report.toString();
+
+
+
+
+
+
