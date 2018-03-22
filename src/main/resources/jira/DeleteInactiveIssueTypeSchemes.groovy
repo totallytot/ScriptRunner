37fd7f51d643b83c35c94416792bee5fe449bc59
AbstarctElement <fcukthepop@gmail.com>
2018-03-22 12:33:12 +0300

    yo

---
+++ src/main/resources/jira/DeleteInactiveIssueTypeSchemes.groovy
@@ -0,0 +1,22 @@
+package jira
+
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.fields.config.FieldConfigScheme
+import com.atlassian.jira.issue.fields.config.manager.IssueTypeSchemeManager
+
+IssueTypeSchemeManager issueTypeSchemeManager = ComponentAccessor.getIssueTypeSchemeManager();
+List<FieldConfigScheme> fieldConfigSchemes = issueTypeSchemeManager.getAllSchemes();
+StringBuilder report = new StringBuilder();
+
+int i = 0;
+
+fieldConfigSchemes.each {
+
+    if (!it.getName().equals("Default Issue Type Scheme") && it.getAssociatedProjectObjects().size() == 0) {
+        report.append(it.getName());
+        report.append(" | ");
+        i++;
+        issueTypeSchemeManager.deleteScheme(it);
+    }
+}
+return "All:"+ i + " " + report.toString();
