a6f678b3750057922c60ce0778013886972eccfd
Totally Tot <fcukthepop@gmail.com>
2018-06-06 18:57:09 +0300

    final fix

---
+++ src/main/resources/jira/postfunctions/TransitParentIssueOfSubtaskToDoneSC.groovy
@@ -0,0 +1,73 @@
+package jira.postfunctions
+
+import com.atlassian.jira.bc.issue.IssueService
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.IssueInputParameters
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.link.IssueLinkManager
+import com.atlassian.jira.security.JiraAuthenticationContext
+import com.atlassian.jira.user.ApplicationUser
+
+String user = "ext_alexk";
+ApplicationUser applicationUser = ComponentAccessor.getUserManager().getUserByKey(user);
+JiraAuthenticationContext jiraAuthenticationContext = ComponentAccessor.getJiraAuthenticationContext();
+jiraAuthenticationContext.setLoggedInUser(applicationUser);
+
+//for testing in order to catch the issue
+IssueManager issueManager = ComponentAccessor.getIssueManager();
+MutableIssue issue = issueManager.getIssueObject("INFRA-26790");
+
+//take parent issue instead of sub-task and transit it
+MutableIssue parentIssue;
+IssueLinkManager issueLinkManager = ComponentAccessor.getIssueLinkManager();
+issueLinkManager.getInwardLinks(issue.getId()).each {
+    if (it.getIssueLinkType().getId() == 10100) {
+        parentIssue = (MutableIssue) it.getSourceObject();
+        return true;
+    }
+}
+
+//transit parent issue
+boolean shouldtransitParent = false;
+
+if (parentIssue != null) {
+
+    //check if all sub-tasks are in done status
+    int count = 0;
+    issueLinkManager.getOutwardLinks(parentIssue.getId()).each {
+        if (it.getDestinationObject().getStatus().getName().equals("Done")) {
+            count++;
+        }
+    }
+
+    if (issueLinkManager.getOutwardLinks(parentIssue.getId()).size() == count /*+ 1*/) shouldtransitParent = true;
+
+    if (shouldtransitParent) {
+        IssueService issueService = ComponentAccessor.getIssueService();
+        IssueInputParameters issueInputParameters = issueService.newIssueInputParameters();
+        if (parentIssue.getAssignee() == null) issueInputParameters.setAssigneeId(applicationUser.getKey());
+
+        String parentIssueType = parentIssue.getIssueType().getName();
+        int transitionId;
+
+        switch (parentIssueType) {
+            case "Bug":
+            case "Task":
+            case "Technical Debt":
+            //case "Story":
+                transitionId = 101; //61
+                break;
+            case "Production Issue":
+                transitionId = 61;
+                break;
+        }
+
+        IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(applicationUser, parentIssue.getId(), transitionId, issueInputParameters);
+        if (transitionValidationResult.isValid()) {
+            IssueService.IssueResult transitionResult = issueService.transition(applicationUser, transitionValidationResult);
+            return transitionResult.getErrorCollection().each {};
+        }
+    }
+}
+
