149df34c198ed31c65e374cb028819811184e1ed
Totally Tot <fcukthepop@gmail.com>
2018-06-06 16:44:12 +0300

    script for sub-tasks

---
+++ src/main/resources/jira/postfunctions/TransitParentIssueOfSubtaskSC.groovy
@@ -0,0 +1,65 @@
+package jira.postfunctions
+
+import com.atlassian.jira.bc.issue.IssueService
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.IssueInputParameters
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.link.IssueLinkManager
+import com.atlassian.jira.security.JiraAuthenticationContext
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.util.JiraUtils
+import com.atlassian.jira.workflow.WorkflowTransitionUtil
+import com.atlassian.jira.workflow.WorkflowTransitionUtilImpl
+
+String user = "ext_alexk";
+ApplicationUser applicationUser = ComponentAccessor.getUserManager().getUserByKey(user);
+JiraAuthenticationContext jiraAuthenticationContext = ComponentAccessor.getJiraAuthenticationContext();
+jiraAuthenticationContext.setLoggedInUser(applicationUser);
+
+//for testing in order to catch the issue
+IssueManager issueManager = ComponentAccessor.getIssueManager();
+MutableIssue issue = issueManager.getIssueObject("INFRA-26790");
+
+WorkflowTransitionUtil workflowTransitionUtil = (WorkflowTransitionUtil) JiraUtils.loadComponent(WorkflowTransitionUtilImpl.class);
+
+//take parent issue instead of sub-task and transit it
+MutableIssue parentIssue;
+IssueLinkManager issueLinkManager = ComponentAccessor.getIssueLinkManager();
+issueLinkManager.getInwardLinks(issue.getId()).each {
+    if (it.getIssueLinkType().getId() == 10100) {
+        parentIssue = (MutableIssue) it.getSourceObject();
+        return true;
+    }
+}
+
+//transit parent issue
+if (parentIssue != null) {
+    IssueService issueService = ComponentAccessor.getIssueService();
+    IssueInputParameters issueInputParameters = issueService.newIssueInputParameters();
+    if (parentIssue.getAssignee() == null) issueInputParameters.setAssigneeId(applicationUser.getKey());
+
+    String parentIssueType = parentIssue.getIssueType().getName();
+    int transitionId = null;
+
+    switch (parentIssueType) {
+        case "Bug":
+        case "Task":
+        case "Technical Debt":
+        case "Story":
+            transitionId = 51;
+            break;
+        case "Production Issue":
+            transitionId = 11;
+            break;
+    }
+
+    IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(applicationUser, parentIssue.getId(), transitionId, issueInputParameters);
+    if (transitionValidationResult.isValid()) {
+        IssueService.IssueResult transitionResult = issueService.transition(applicationUser, transitionValidationResult);
+        return transitionResult.getErrorCollection().each {};
+    }
+}
+
+
+
