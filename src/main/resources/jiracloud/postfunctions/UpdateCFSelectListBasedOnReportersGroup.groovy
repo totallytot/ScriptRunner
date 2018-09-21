6119652dee93b8a6d9eb9dc199f6bc47fbbeb16f
Totally Tot <fcukthepop@gmail.com>
2018-09-21 11:05:35 +0300

    condition for empty field

--- src/main/resources/jiracloud/postfunctions/UpdateCFSelectListBasedOnReportersGroup.groovy
+++ src/main/resources/jiracloud/postfunctions/UpdateCFSelectListBasedOnReportersGroup.groovy
@@ -1,5 +1,10 @@
 package jiracloud.postfunctions
 
+//condition that starts script if the field is empty
+def value = issue.fields["customfield_10036"] as String
+return value == null
+
+//script
 def issueEditMeta = get("/rest/api/2/issue/" + issue.key + "/editmeta").asObject(Map)
 def options = issueEditMeta.body.fields.customfield_10036.allowedValues.value
 def userGroups = get("/rest/api/2/user/groups?key=" + issue.fields.reporter.key).asObject(List).body.name
