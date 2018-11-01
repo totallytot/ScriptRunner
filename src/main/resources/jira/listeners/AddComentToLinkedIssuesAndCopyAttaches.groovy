4ca1497231efb3de7ad239af951f7c34b7f7695d
Totally Tot <fcukthepop@gmail.com>
2018-11-01 23:03:14 +0300

    copy attachments

--- src/main/resources/jira/listeners/AddComentToLinkedIssuesAndCopyAttaches.groovy
+++ src/main/resources/jira/listeners/AddComentToLinkedIssuesAndCopyAttaches.groovy
@@ -1,10 +1,11 @@
 package jira.listeners
 
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.MutableIssue
 
-def linker = ComponentAccessor.issueLinkManager
 def commentMgr = ComponentAccessor.commentManager
-def issue = event.issue
+def issue = event.issue as MutableIssue
+
 
 // gather the original author and comment body from the original issues comment
 def type = issue.issueType.name
@@ -13,8 +14,11 @@ def originalAuthor = newComment.authorApplicationUser
 def commentBody = newComment.body
 
 // get original issue's linked issues and create comment on the linked issue
-if (commentBody != null && type == "Service Request") {
-    linker.getOutwardLinks(issue.id).each {
+if (commentBody && type == "Service Request") {
+        def attacher = ComponentAccessor.attachmentManager
+        def linker = ComponentAccessor.issueLinkManager
+        linker.getOutwardLinks(issue.id).each {
         commentMgr.create(it.destinationObject, originalAuthor, commentBody , true)
+        if (attacher.getAttachments(issue)) attacher.copyAttachments(issue, originalAuthor, it.destinationObject.key)
     }
 }
