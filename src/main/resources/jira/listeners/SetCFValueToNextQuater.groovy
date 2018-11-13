f037f19f137788d188818a40f66d848d30dcb8bc
darmarius <darmarius123@gmail.com>
2018-11-13 18:34:15 +0300

    transit issue by anchor CF

--- src/main/resources/jira/listeners/SetCFValueToNextQuater.groovy
+++ src/main/resources/jira/listeners/SetCFValueToNextQuater.groovy
@@ -1,12 +1,14 @@
 
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+
 import java.text.SimpleDateFormat
 
-def issue = event.issue
+Issue issue = event.issue
 def sprintField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Sprint")
 def startDate = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(16911L)
 Date date = new Date()
-if (issue.getIssueType().getName()!="Sub-task" && issue.getStatus().getStatusCategory().getName()!="Complete" && sprintField.getValue(issue) == null && startDate.getValue(issue)<date){
+if (!issue.isSubTask() && issue.getStatus().getStatusCategory().getName()!="Complete" && sprintField.getValue(issue) == null && startDate.getValue(issue)<date){
     def spField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(10002L)
     int sp
     if (spField.getValue(issue)!=null) sp = (int)spField.getValue(issue)
@@ -82,3 +84,111 @@ void updateDateCfWithHistory(String stringDate, Long cf){
         issueService.update(applicationUser, validationResult).hasWarnings()
     }
 }
+
+
+///console version
+
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.component.ComponentAccessor
+import java.text.SimpleDateFormat
+
+Collection<Long> issues = new ArrayList()
+List<String> projetsKeys = new ArrayList()
+projetsKeys.add("CIT")
+projetsKeys.add("MGP")
+projetsKeys.add("IRIS")
+projetsKeys.each {
+    def project = ComponentAccessor.getProjectManager().getProjectObjByKey(it)
+    issues.addAll(ComponentAccessor.getIssueManager().getIssueIdsForProject(project.getId()));
+}
+issues.each {
+    Issue issue = ComponentAccessor.getIssueManager().getIssueObject(it)
+    def sprintField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Sprint")
+    def startDate = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(16911L)
+    Date date = new Date()
+    if (!issue.isSubTask()  && issue.getStatus().getStatusCategory().getName()!="Complete" && sprintField.getValue(issue) == null && (Date)startDate.getValue(issue)<date){
+        def spField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(10002L)
+        int sp
+        if (spField.getValue(issue)!=null) sp = (int)spField.getValue(issue)
+        else sp = 1
+        Calendar createdDate = Calendar.getInstance()
+        def month =  createdDate.get(Calendar.MONTH)
+        switch (month) {
+            case 0:
+                setQuater(createdDate,3,sp,issue)
+                break;
+            case 1:
+                setQuater(createdDate,3,sp,issue)
+                break;
+            case 2:
+                setQuater(createdDate,3,sp,issue)
+                break;
+            case 3:
+                setQuater(createdDate,6,sp,issue)
+                break;
+            case 4:
+                setQuater(createdDate,6,sp,issue)
+                break;
+            case 5:
+                setQuater(createdDate,6,sp,issue)
+                break;
+            case 6:
+                setQuater(createdDate,9,sp,issue)
+                break;
+            case 7:
+                setQuater(createdDate,9,sp,issue)
+                break;
+            case 8:
+                setQuater(createdDate,9,sp,issue)
+                break;
+            case 9:
+                setQuater(createdDate,0,sp,issue)
+                break;
+            case 10:
+                setQuater(createdDate,0,sp,issue)
+                break;
+            case 11:
+                setQuater(createdDate,0,sp,issue)
+                break;
+            default:
+                return;
+        }
+
+    }
+
+}
+
+void setQuater(Calendar createdDate, int mounthOfQuater, int sp, Issue issue){
+    int year =  createdDate.get(Calendar.YEAR)
+    if (mounthOfQuater == 0) year++
+    def date = new Date(year, mounthOfQuater,1)
+    def dateFormat = new SimpleDateFormat("dd/MMM/YY");
+    def startDate = dateFormat.format(date)
+    def endDate = dateFormat.format(date.plus(sp))
+    updateDateCfWithHistory (startDate, 16911L, issue)
+    updateDateCfWithHistory (endDate, 16912L, issue)
+}
+
+
+void updateDateCfWithHistory(String stringDate, Long cf, Issue issue){
+    def updateDate = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(cf)
+    def user = "tech_user"
+    def applicationUser = ComponentAccessor.getUserManager().getUserByKey(user)
+    def issueService = ComponentAccessor.getIssueService()
+    def issueInputParameters = issueService.newIssueInputParameters()
+    issueInputParameters.addCustomFieldValue(updateDate.getIdAsLong(), stringDate)
+    def validationResult = issueService.validateUpdate(applicationUser, issue.getId(), issueInputParameters)
+    if (validationResult.isValid()) {
+
+        issueService.update(applicationUser, validationResult).hasWarnings()
+    }
+}
+
+
+
+
+
+
+
+
+
--- src/main/resources/jira/listeners/SetCFValuesBasedOnDueDate.groovy
+++ src/main/resources/jira/listeners/SetCFValuesBasedOnDueDate.groovy
@@ -40,3 +40,49 @@ void updateDateCfWithHistory(String stringDate, Long cf){
         }
     }
 }
+
+//// console version
+import com.atlassian.jira.component.ComponentAccessor
+import java.text.SimpleDateFormat
+
+def project = ComponentAccessor.getProjectManager().getProjectObjByKey("NOCPR")
+def issues = ComponentAccessor.getIssueManager().getIssueIdsForProject(project.getId())
+issues.each {
+    def issue = ComponentAccessor.getIssueManager().getIssueObject(it)
+    def dateFormat = new SimpleDateFormat("dd/MMM/YY");
+    if (issue.getStatus().getStatusCategory().getName()!= "Complete" && issue.getDueDate() != null){
+        def dueDate = issue.getDueDate().minus(10)
+        def endDate = dateFormat.format(issue.dueDate)
+        updateDateCfWithHistory (endDate, 16912L, issue)
+        def origEstimate = issue.getOriginalEstimate()
+        def startDate
+        if (origEstimate != null ){
+            origEstimate/=28800
+            origEstimate+=10
+            startDate = dateFormat.format(issue.getDueDate().minus((int)Math.round(origEstimate)))
+            updateDateCfWithHistory (startDate, 16911L,issue)
+        }
+        else{
+            startDate = dateFormat.format(issue.getDueDate().minus(11))
+            updateDateCfWithHistory (startDate, 16911L,issue)
+        }
+    }
+}
+
+void updateDateCfWithHistory(String stringDate, Long cf, def issue){
+
+
+    def updateDate = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(cf)
+    if (updateDate.getValue(issue) == null)
+    {
+        def user = "tech_user"
+        def applicationUser = ComponentAccessor.getUserManager().getUserByKey(user)
+        def issueService = ComponentAccessor.getIssueService()
+        def issueInputParameters = issueService.newIssueInputParameters()
+        issueInputParameters.addCustomFieldValue(updateDate.getIdAsLong(), stringDate)
+        def validationResult = issueService.validateUpdate(applicationUser, issue.getId(), issueInputParameters)
+        if (validationResult.isValid()) {
+            issueService.update(applicationUser, validationResult).hasWarnings()
+        }
+    }
+}
