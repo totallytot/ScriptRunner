d0210d876dd36916753e1bd304c38acc255d6e38
Totally Tot <fcukthepop@gmail.com>
2018-11-28 17:50:36 +0300

    Condition for linked issues

--- src/main/resources/jira/validators/LinkedIssueFromCertainProjectInClosedStatus.groovy
+++ src/main/resources/jira/validators/LinkedIssueFromCertainProjectInClosedStatus.groovy
@@ -2,14 +2,14 @@ package jira.validators
 
 import com.atlassian.jira.component.ComponentAccessor
 
-//On the project HEPROD - allow HEC Request to progress 'To Approval' Only if it has a linked HEC Request in VAL and if that one is 'Closed'
+//On the project HEPROD - allow HEC Request to progress 'To Approval' Only if it has a linked HEC Request.
+//Link type should be 'VAL HECReq reference'. The behavior should be so that issues linked in this way are 'Closed'.
 
 if (issue.key.contains("HEPROD-")) {
-    def issueLinkManager = ComponentAccessor.issueLinkManager
-    issueLinkManager.getOutwardLinks(issue.id).any {
-        it.destinationObject.key.contains("HEVAL-") &&
-                it.destinationObject.issueType.name == "HEC Request" &&
-                it.destinationObject.status.name == "Closed"
+    def VALHECReqReference = ComponentAccessor.issueLinkManager.getOutwardLinks(issue.id).findAll {
+        it.linkTypeId == 10400 && it.destinationObject.key.contains("HEVAL-") &&
+                it.destinationObject.issueType.name == "HEC Request"
     }
-} else return true
-
+        if (VALHECReqReference) VALHECReqReference.every {it.destinationObject.status.name == "Closed"}
+        else false
+} else true
