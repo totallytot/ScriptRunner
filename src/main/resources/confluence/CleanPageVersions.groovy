c98208dd6baff4ee3983bf0c443543bbbdbcec58
Totally Tot <fcukthepop@gmail.com>
2019-02-27 11:07:09 +0300

    page versions update

--- src/main/resources/confluence/CleanPageVersions.groovy
+++ src/main/resources/confluence/CleanPageVersions.groovy
@@ -1,32 +1,30 @@
 package confluence
 
-import com.atlassian.confluence.pages.Page;
-import com.atlassian.confluence.pages.PageManager;
-import com.atlassian.confluence.spaces.Space;
-import com.atlassian.confluence.spaces.SpaceManager;
-import com.atlassian.spring.container.ContainerManager;
+import com.atlassian.confluence.pages.Page
+import com.atlassian.confluence.pages.PageManager
+import com.atlassian.confluence.spaces.SpaceManager
+import com.atlassian.spring.container.ContainerManager
 
-PageManager pageManager = (PageManager) ContainerManager.getInstance().getComponent("pageManager");
-SpaceManager spaceManager = (SpaceManager) ContainerManager.getInstance().getComponent("spaceManager");
+def pageManager = (PageManager) ContainerManager.getInstance().getComponent("pageManager")
+def spaceManager = (SpaceManager) ContainerManager.getInstance().getComponent("spaceManager")
 
-Space space = spaceManager.getSpace("EPMBDASFC");
-List<Page> pages = pageManager.getPages(space, true); //boolean currentOnly
+def space = spaceManager.getSpace("SPACEKEY")
+def pages = pageManager.getPages(space, true) //boolean currentOnly
 
-int versionsToLeave = 3;
+def versionsToLeave = 0
+def removedPagesTitles = []
 
-for (Page page : pages) {
-    int previousVersion = page.getPreviousVersion();
-
+pages.each {
+    int previousVersion = it.getPreviousVersion()
     if (previousVersion > versionsToLeave) {
-
         for (int i = 1; i <= (previousVersion - versionsToLeave); i++) {
-
             try {
-                Page pageForRemoval = (Page) pageManager.getPageByVersion(page, i);
-                pageForRemoval.remove(pageManager);
+                def pageForRemoval = (Page) pageManager.getPageByVersion(it, i)
+                removedPagesTitles << "title: " + pageForRemoval.displayTitle + " - version: " + pageForRemoval.version
             } catch (NullPointerException e) {
-                //do nothing
+                log.error(e.message)
             }
         }
     }
 }
+removedPagesTitles.size()
