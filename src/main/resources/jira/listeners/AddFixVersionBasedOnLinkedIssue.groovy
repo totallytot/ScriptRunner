4bcdfb33ecef8c4d4b9cd7d45a715e56847b552e
Totally Tot <fcukthepop@gmail.com>
2019-03-20 13:37:57 +0300

    Listener for fixVersions update

--- src/main/resources/jira/listeners/AddFixVersionBasedOnLinkedIssue.groovy
+++ src/main/resources/jira/listeners/AddFixVersionBasedOnLinkedIssue.groovy
@@ -3,40 +3,39 @@ package jira.listeners
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.event.issue.link.IssueLinkCreatedEvent
 import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.index.IssueIndexingService
+import com.atlassian.jira.util.ImportUtils
 
 def event = event as IssueLinkCreatedEvent
 def linkName = event.issueLink.issueLinkType.name
 def sourceIssue = event.issueLink.sourceObject as Issue //Test
-def destinationIssue = event.issueLink.destinationObject as Issue//SV
+def destinationIssue = event.issueLink.destinationObject as Issue//Specification Version
 
 if (linkName == "Tests" && sourceIssue.issueType.name == "Test" && destinationIssue.issueType.name == "Specification Version") {
     def versionManager = ComponentAccessor.versionManager
-    def sourceFixVersions = sourceIssue.fixVersions
-    def destinationFixVersions = destinationIssue.fixVersions
-    def sourceProjectFixVersions = sourceIssue.projectObject.versions
-    def sourceProjectFixVersionsNames = sourceIssue.projectObject.versions.collect{it.name}
-    def destinationIssueFixVersionsNames = destinationIssue.fixVersions.collect {it.name}
+    def sourceProjectFixVersionsNames = sourceIssue.projectObject.versions.collect { it.name }
+    def destinationIssueFixVersionsNames = destinationIssue.fixVersions.collect { it.name }
+    def versionsForUpdate = []
 
     //no common fix version names
     if (sourceProjectFixVersionsNames.disjoint(destinationIssueFixVersionsNames)) {
-        def newVersions = []
-        destinationFixVersions.each{ fixVersion ->
-            newVersions << versionManager.createVersion(fixVersion.name, fixVersion.startDate, fixVersion.releaseDate,
+        destinationIssue.fixVersions.each { fixVersion ->
+            versionsForUpdate << versionManager.createVersion(fixVersion.name, fixVersion.startDate, fixVersion.releaseDate,
                     fixVersion.description, sourceIssue.projectId, null, fixVersion.released)
         }
-        newVersions.addAll(sourceFixVersions)
-        versionManager.updateIssueFixVersions(sourceIssue, newVersions)
         //common fix version names
     } else {
-        def commonVersionsByName = []
-        commonVersionsByName = sourceProjectFixVersions?.findAll{ it.name in destinationIssueFixVersionsNames }
-        def versionsForCreation  = destinationFixVersions.findAll{ !sourceProjectFixVersionsNames.contains(it.name) }
-
-        versionsForCreation.each{ fixVersion ->
-            commonVersionsByName << versionManager.createVersion(fixVersion.name, fixVersion.startDate, fixVersion.releaseDate,
+        versionsForUpdate = sourceIssue.projectObject.versions?.findAll { it.name in destinationIssueFixVersionsNames }
+        def versionsForCreation = destinationIssue.fixVersions.findAll { !sourceProjectFixVersionsNames.contains(it.name) }
+        versionsForCreation.each { fixVersion ->
+            versionsForUpdate << versionManager.createVersion(fixVersion.name, fixVersion.startDate, fixVersion.releaseDate,
                     fixVersion.description, sourceIssue.projectId, null, fixVersion.released)
         }
-        commonVersionsByName.addAll(sourceFixVersions)
-        versionManager.updateIssueFixVersions(sourceIssue, commonVersionsByName)
     }
+    versionsForUpdate.addAll(sourceIssue.fixVersions)
+    versionManager.updateIssueFixVersions(sourceIssue, (Collection) versionsForUpdate)
+    boolean wasIndexing = ImportUtils.isIndexIssues()
+    ImportUtils.setIndexIssues(true)
+    ComponentAccessor.getComponent(IssueIndexingService.class).reIndex(sourceIssue)
+    ImportUtils.setIndexIssues(wasIndexing)
 }
