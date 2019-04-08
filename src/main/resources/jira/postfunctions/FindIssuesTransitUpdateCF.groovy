583ff871e42484ec0c07d10023e64e7490bfaaef
Totally Tot <fcukthepop@gmail.com>
2019-04-08 10:52:33 +0300

    pf update

--- src/main/resources/jira/postfunctions/FindIssuesTransitUpdateCF.groovy
+++ src/main/resources/jira/postfunctions/FindIssuesTransitUpdateCF.groovy
@@ -10,7 +10,7 @@ def issueManager = ComponentAccessor.issueManager
 def smartClonedFromField = ComponentAccessor.customFieldManager.getCustomFieldObject(10400)
 def inProductTestField = ComponentAccessor.customFieldManager.getCustomFieldObject(10500)
 def transitionId = 171
-
+def maxAllowedLoop = 60
 def updateStringField = { CustomField field, MutableIssue localIssue, String value ->
     def inputParameters = issueService.newIssueInputParameters()
     inputParameters.addCustomFieldValue(field.id, value).setSkipScreenCheck(true)
@@ -34,14 +34,16 @@ def transitIssueToReplace = { MutableIssue localIssue ->
 
 updateStringField(inProductTestField, issue, issue.key)
 def issueForTransition = issueManager?.getIssueObject(smartClonedFromField?.getValueFromIssue(issue) as String)
+def counter = 0
 
 if (issueForTransition) {
+    counter++
     transitIssueToReplace(issueForTransition)
     updateStringField(inProductTestField, issueForTransition, issue.key)
     def isRunning = true
     while (isRunning) {
         issueForTransition = issueManager?.getIssueObject(issueForTransition?.getCustomFieldValue(smartClonedFromField) as String)
-        if (issueForTransition == null || issueForTransition.key == issue.key) isRunning = false
+        if (issueForTransition == null || issueForTransition.key == issue.key || counter > maxAllowedLoop) isRunning = false
         if (issueForTransition) {
             transitIssueToReplace(issueForTransition)
             updateStringField(inProductTestField, issueForTransition, issue.key)
