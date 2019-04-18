25d2011f241d1e34e76e93e80103cfbd537e996b
Totally Tot <fcukthepop@gmail.com>
2019-04-18 18:20:10 +0300

    pf update

--- src/main/resources/jira/postfunctions/TransitSubTaskFromSubTask.groovy
+++ src/main/resources/jira/postfunctions/TransitSubTaskFromSubTask.groovy
@@ -2,19 +2,17 @@ package jira.postfunctions
 
 import com.atlassian.jira.bc.issue.IssueService
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
 
 def parentIssue = issue.parentObject
-def subTaskIssueToTransit = parentIssue.subTaskObjects.findAll{it.status.name == "Released"}
-
-if (subTaskIssueToTransit) {
-    def issueService = ComponentAccessor.issueService
-    def currentUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
-    subTaskIssueToTransit.each{
-        def issueInputParameters = issueService.newIssueInputParameters()
-        IssueService.TransitionValidationResult transitionValidationResult =
-                issueService.validateTransition(currentUser, it.id, 321, issueInputParameters)
-        if (transitionValidationResult.isValid()) {
-            issueService.transition(currentUser, transitionValidationResult)
-        }
+def issueService = ComponentAccessor.issueService
+def currentUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+parentIssue.subTaskObjects.findAll { it.status.name == "Released" }?.each { issue ->
+    issue = issue as Issue
+    def issueInputParameters = issueService.newIssueInputParameters()
+    IssueService.TransitionValidationResult transitionValidationResult =
+            issueService.validateTransition(currentUser, issue.id, 321, issueInputParameters)
+    if (transitionValidationResult.isValid()) {
+        issueService.transition(currentUser, transitionValidationResult)
     }
 }
