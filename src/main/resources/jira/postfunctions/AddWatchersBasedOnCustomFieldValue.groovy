62b749760a2f0288ebf952677813a8f3e42b18be
RFiKT <hiva.vlad@gmail.com>
2019-06-04 15:26:04 +0300

    Create AddWatchersBasedOnCustomFieldValue.groovy

---
+++ src/main/resources/jira/postfunctions/AddWatchersBasedOnCustomFieldValue.groovy
@@ -0,0 +1,18 @@
+import com.atlassian.jira.component.ComponentAccessor
+
+def watcherManager = ComponentAccessor.watcherManager
+def userManager = ComponentAccessor.userManager
+def cfManager = ComponentAccessor.customFieldManager
+def cf = cfManager.getCustomFieldObject("customfield_ID")
+def value = issue.getCustomFieldValue(cf)
+
+def watchUsers = {usernames ->
+    usernames.each {
+        def user = userManager.getUserByKey(it.toString())
+        watcherManager.startWatching(user, issue)
+    }
+}
+if (value.toString() == "Value"){
+    def users = ["username","comma","separated"]
+    watchUsers(users)
+}
