8e1eb2bfd0edf9012ce96fb13b6761c5062e4a22
Totally Tot <fcukthepop@gmail.com>
2019-07-05 20:35:24 +0300

    new condition added

--- src/main/resources/jira/postfunctions/CopyFieldsToCertainIssueInEpicAndTransitIt.groovy
+++ src/main/resources/jira/postfunctions/CopyFieldsToCertainIssueInEpicAndTransitIt.groovy
@@ -7,12 +7,8 @@ import com.atlassian.jira.user.DelegatingApplicationUser
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.issue.link.IssueLinkTypeManager
 
-//for testing in console
+//for testing in console only
 def issue = ComponentAccessor.issueManager.getIssueObject("JI-2")
-
-//user for the script
-def user = "auto"
-def applicationUser = ComponentAccessor.getUserManager().getUserByKey(user)
 
 def customFieldObjects = ComponentAccessor.customFieldManager.getCustomFieldObjects(issue)
 def externalPartnerValue = customFieldObjects.find {
@@ -26,13 +22,14 @@ def externalModellingIssue = ComponentAccessor.issueLinkManager.getOutwardLinks(
 }.collect { it.destinationObject }.find { it.issueType.name == "External Modelling" }
 
 //fields to copy
-def productDataUrlField = customFieldObjects.find { it.name == "Product Data URL" } //text
+def productDataUrlField = customFieldObjects.find { it.name == "Product Data URL" } //url
 def dimensionsField = customFieldObjects.find { it.name == "Dimensions" } //text
 def geometryVariationsField = customFieldObjects.find { it.name == "Geometry Variations" } //text
 def modelingComplexityField = customFieldObjects.find { it.name == "Modeling Complexity" } //select list
 def modelingComplexityValue = issue.getCustomFieldValue(modelingComplexityField) as LazyLoadedOption
 
 if (externalModellingIssue) {
+    def applicationUser = ComponentAccessor.getUserManager().getUserByKey("auto")
     def issueService = ComponentAccessor.issueService
     def issueInputParameters = issueService.newIssueInputParameters()
     issueInputParameters.setAssigneeId(externalPartnerValue.key)
@@ -41,7 +38,8 @@ if (externalModellingIssue) {
             .addCustomFieldValue(geometryVariationsField.idAsLong, geometryVariationsField.getValue(issue).toString())
             .addCustomFieldValue(modelingComplexityField.idAsLong, modelingComplexityValue.optionId.toString())
             .setSkipScreenCheck(true)
-    IssueService.UpdateValidationResult validationResult = issueService.validateUpdate(applicationUser,
-            externalModellingIssue.getId(), issueInputParameters)
-    if (validationResult.valid) issueService.update(applicationUser, validationResult)
+    IssueService.TransitionValidationResult transitionValidationResult = issueService.
+            validateTransition(applicationUser, externalModellingIssue.getId(), 91, issueInputParameters)
+    if (transitionValidationResult.isValid()) issueService.transition(applicationUser, transitionValidationResult)
+    else transitionValidationResult.errorCollection
 }
