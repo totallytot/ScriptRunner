5ff9955f85b04734d5fdcaf979c5fd23e2679427
Totally Tot <fcukthepop@gmail.com>
2019-07-19 15:49:08 +0300

    new condition added

---
+++ src/main/resources/jira/postfunctions/sub_task_creation/BasedOnSingleSelectListValue.groovy
@@ -0,0 +1,49 @@
+package jira.postfunctions.sub_task_creation
+
+import com.atlassian.jira.component.ComponentAccessor
+
+// for testing in script console
+def issue = ComponentAccessor.issueManager.getIssueObject("ITSDTEST-2")
+
+def subTaskId = 10101
+def issueService = ComponentAccessor.issueService
+def subTaskManager = ComponentAccessor.subTaskManager
+def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+def teamRoleValue = ComponentAccessor.customFieldManager.getCustomFieldObjects(issue).find {
+    it.id == "customfield_16920"
+}.getValue(issue).toString()
+
+def general = ["Jira", "Active Directory (Network Account)", "Office", "OpenAir", "LMS", "Concur"]
+def finance = general.collect() << "NetSuite"
+def engineering = general.collect() << "Wiki"
+
+def createSubTask = { summary ->
+    def subTaskInputParameters = issueService.newIssueInputParameters()
+    subTaskInputParameters
+            .setSummary(summary as String)
+            .setIssueTypeId(subTaskId as String)
+            .setReporterId(user.key)
+            .setProjectId(issue.projectId)
+            .setPriorityId(issue.priority.id)
+            .setSkipScreenCheck(true)
+    def createValidationResult = issueService.validateSubTaskCreate(user, issue.id, subTaskInputParameters)
+    if (createValidationResult.valid) {
+        def subTask = issueService.create(user, createValidationResult).issue
+        subTaskManager.createSubTaskIssueLink(issue, subTask, user)
+    }
+    else createValidationResult.errorCollection
+}
+
+switch (teamRoleValue) {
+    case "General":
+        general.each{ createSubTask(it)}
+        break
+    case "Finance":
+        finance.each{createSubTask(it)}
+        break
+    case "Engineering":
+        engineering.each{createSubTask(it)}
+        break
+    default:
+        break
+}
