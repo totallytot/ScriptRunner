a114b7638bc3dced1a979100a1a708fa3df6f93c
TotallyTot <fcukthepop@gmail.com>
2019-09-05 19:56:15 +0300

    minor fix

--- src/main/resources/jira/listeners/UpdateLableInLinkedIssues.groovy
+++ src/main/resources/jira/listeners/UpdateLableInLinkedIssues.groovy
@@ -8,19 +8,21 @@ import org.ofbiz.core.entity.GenericValue
 
 def issueUpdateEvent = event as IssueEvent
 def issue = issueUpdateEvent.issue
-GenericValue change = issueUpdateEvent?.changeLog?.getRelated("ChildChangeItem")?.find {  genericValue ->
-    genericValue.get("field") == "labels"
-}
-if (change) {
-    def oldLabels = change.get("oldstring").toString().split()
-    def newLabels = change.get("newstring").toString().split()
-    def labelsForLinkedIssues = newLabels.findAll { !(it in oldLabels)}
-    if (issue.issueType.name == "Story") {
-        def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
-        def labelManager = ComponentAccessor.getComponent(LabelManager.class)
-        ComponentAccessor.issueLinkManager.getOutwardLinks(issue.id).each { IssueLink issueLink ->
-            labelsForLinkedIssues.each {
-                labelManager.addLabel(user, issueLink.destinationObject.id, it, false)
+if (issue.issueType.name == "Story") {
+    GenericValue change = issueUpdateEvent?.changeLog?.getRelated("ChildChangeItem")?.find { genericValue ->
+        genericValue.get("field") == "labels"
+    }
+    if (change) {
+        def oldLabels = change.get("oldstring").toString().split()
+        def newLabels = change.get("newstring").toString().split()
+        def labelsForLinkedIssues = newLabels.findAll { !(it in oldLabels) }
+        if (labelsForLinkedIssues) {
+            def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+            def labelManager = ComponentAccessor.getComponent(LabelManager.class)
+            ComponentAccessor.issueLinkManager.getOutwardLinks(issue.id).each { IssueLink issueLink ->
+                labelsForLinkedIssues.each {
+                    labelManager.addLabel(user, issueLink.destinationObject.id, it, false)
+                }
             }
         }
     }
