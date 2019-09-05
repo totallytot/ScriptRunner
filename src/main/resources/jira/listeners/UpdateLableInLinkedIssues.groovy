510854f30eb29cdea2eab7429b7e67325f165f34
TotallyTot <fcukthepop@gmail.com>
2019-09-05 19:49:14 +0300

    minor fix

--- src/main/resources/jira/listeners/UpdateLableInLinkedIssues.groovy
+++ src/main/resources/jira/listeners/UpdateLableInLinkedIssues.groovy
@@ -4,13 +4,10 @@ import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.event.issue.IssueEvent
 import com.atlassian.jira.issue.label.LabelManager
 import com.atlassian.jira.issue.link.IssueLink
-import org.apache.log4j.Level
-import org.apache.log4j.Logger
 import org.ofbiz.core.entity.GenericValue
 
-def log = Logger.getLogger("check-me-dude")
-log.setLevel(Level.DEBUG)
 def issueUpdateEvent = event as IssueEvent
+def issue = issueUpdateEvent.issue
 GenericValue change = issueUpdateEvent?.changeLog?.getRelated("ChildChangeItem")?.find {  genericValue ->
     genericValue.get("field") == "labels"
 }
@@ -18,7 +15,6 @@ if (change) {
     def oldLabels = change.get("oldstring").toString().split()
     def newLabels = change.get("newstring").toString().split()
     def labelsForLinkedIssues = newLabels.findAll { !(it in oldLabels)}
-    def issue = ComponentAccessor.issueManager.getIssueObject("PFT-54")
     if (issue.issueType.name == "Story") {
         def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
         def labelManager = ComponentAccessor.getComponent(LabelManager.class)
