a90b37f8cb8baf77c8d241040af76e8c0396606b
darmarius <darmarius123@gmail.com>
2019-10-07 14:12:06 +0300

    Create AddRemoteLinkFromCFtoIssue.groovy

---
+++ src/main/resources/jira/postfunctions/AddRemoteLinkFromCFtoIssue.groovy
@@ -0,0 +1,51 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.link.IssueLink;
+import com.atlassian.jira.issue.link.RemoteIssueLinkBuilder
+import com.atlassian.jira.issue.link.RemoteIssueLink
+import com.atlassian.jira.issue.link.RemoteIssueLinkManager
+import com.atlassian.jira.issue.link.IssueLinkTypeManager
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import com.atlassian.jira.component.pico.ComponentManager
+import com.atlassian.jira.issue.ModifiedValue
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+RemoteLinks script = new RemoteLinks()
+script.run(issue)
+
+class RemoteLinks {
+    Logger log = Logger.getLogger("Create Remote links to Issue")
+    long ScriptStartTime = System.currentTimeMillis()
+    def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+    def issueManager = ComponentAccessor.getIssueManager()
+    def strCFIdIssueLinks = "Issue Links" 
+    CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager()
+    void run(MutableIssue issue) {
+        log.setLevel(Level.DEBUG)
+        log.debug("${issue.key} Script started")
+        long ScriptStartTime = System.currentTimeMillis()
+        CustomField linksCF = customFieldManager.getCustomFieldObjectsByName(strCFIdIssueLinks).iterator().next()
+        def stringlinks =  issue.getCustomFieldValue(linksCF)
+        def links = stringlinks.split()
+        for (slink in links){
+            if(slink != "")
+            {
+                try{
+                    def linkBuilder = new RemoteIssueLinkBuilder()
+                    linkBuilder.issueId(issue.id)
+                    linkBuilder.relationship("ECM")
+                    linkBuilder.title("Vendor site")
+                    linkBuilder.url(slink)
+                    def link = linkBuilder.build() 
+                    ComponentAccessor.getComponent(RemoteIssueLinkManager).createRemoteIssueLink(link, user)
+                }catch (Exception ex) {}
+            }
+        }
+        long ScriptWorkTime = System.currentTimeMillis() - ScriptStartTime
+        log.debug("${issue.key} Script work time: ${ScriptWorkTime} ms.")
+    }
+}
