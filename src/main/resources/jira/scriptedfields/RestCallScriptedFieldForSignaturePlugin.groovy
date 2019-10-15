04714d85b706790d7909daa52a4745f9b8034cf3
Totally Tot <fcukthepop@gmail.com>
2019-10-15 18:16:33 +0300

    ScriptedField Added

---
+++ src/main/resources/jira/scriptedfields/RestCallScriptedFieldForSignaturePlugin.groovy
@@ -0,0 +1,31 @@
+package jira.scriptedfields
+
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.Method.GET
+import static groovyx.net.http.Method.POST
+import static groovyx.net.http.ContentType.JSON
+import com.atlassian.jira.component.ComponentAccessor
+import java.text.SimpleDateFormat
+
+def f = new SimpleDateFormat("dd MMM yyyy HH:mm")
+def authRequest = new HTTPBuilder("https://amersham-jira.radbeeqms.com/rest/auth/1/session")
+def authResponse = authRequest.request(POST,JSON) { req ->
+    body = [username: 'auto', password: 'secret']
+    req.setHeader("Content-Type", "application/json")
+}
+def signaturesRequest = new HTTPBuilder("https://amersham-jira.radbeeqms.com/rest/electronic-signature/1.0/signature/${issue.key}")
+def signatures = signaturesRequest.request(GET,JSON) { req ->
+    req.setHeader("Content-Type", "application/json")
+    req.setHeader("Cookie", "JSESSIONID="+authResponse.get("session").get("value").toString())
+}
+if (signatures) {
+// wiki markup style was used in JMCF (not scriptrunner scripted fields)
+    String output = "||User||Date||From Status||To Status||\n"
+    signatures.each {
+        output += "|" + ComponentAccessor.userManager.getUserByKey(it.get("userKey").toString()).getDisplayName() + "|"
+        output += f.format(new Date((it.get("date")))).toString() + "|"
+        output += it.get("fromStatus").toString() + "|"
+        output += it.get("toStatus").toString() + "|\n"
+    }
+    output
+}
