3cd5ebabb05966372522a8c7bcf85a52a6026b1b
Totally Tot <fcukthepop@gmail.com>
2019-12-05 18:14:47 +0300

    Minor fixes and template update

--- src/main/resources/jira/Templates.groovy
+++ src/main/resources/jira/Templates.groovy
@@ -1,8 +1,10 @@
 package jira
 
+import com.atlassian.jira.bc.issue.IssueService
 import com.atlassian.jira.bc.issue.search.SearchService
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.IssueInputParameters
 import com.atlassian.jira.issue.label.LabelManager
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.web.bean.PagerFilter
@@ -21,6 +23,19 @@ static updateIssueLabels(ApplicationUser executionUser, String label, Issue issu
     def sendNotification = false
     def issueUpdateEventAndReindex = true
     labelManager.setLabels(executionUser, issue.id, labelsToSet, sendNotification, issueUpdateEventAndReindex)
+}
+
+//not ready
+static updateMultiUserPicker(ApplicationUser executionUser, Issue issue, String fieldName, String... userKeys) {
+    def multiUserPicker = ComponentAccessor.customFieldManager.getCustomFieldObject(fieldName)
+    def issueService = ComponentAccessor.issueService
+    def issueInputParameters = issueService.newIssueInputParameters().with {
+        addCustomFieldValue(multiUserPicker.id, userKeys).setSkipScreenCheck(true)
+    } as IssueInputParameters
+    IssueService.UpdateValidationResult validationResult = issueService.validateUpdate(executionUser,
+            issue.id, issueInputParameters)
+    if (validationResult.valid) issueService.update(executionUser, validationResult)
+    else validationResult.errorCollection
 }
 
 static List<Issue> getIssuesInEpic(Issue epic) {
--- src/main/resources/jira/postfunctions/UpdateUserPickerCF.groovy
+++ src/main/resources/jira/postfunctions/UpdateUserPickerCF.groovy
@@ -1,4 +1,4 @@
-package jira.postfunctions.console_tests
+package jira.postfunctions
 
 import com.atlassian.jira.bc.issue.IssueService
 import com.atlassian.jira.component.ComponentAccessor
