2befaa61997833915b3cbedf9524ddec70c24747
TotallyTot <fcukthepop@gmail.com>
2020-04-10 15:13:52 +0300

    new scripted field added

---
+++ src/main/resources/jira/scripted_fields/OrigEstMinusRemEst.groovy
@@ -0,0 +1,7 @@
+package jira.scripted_fields
+
+// Template: Duration (time-tracking)
+def origEst = issue.originalEstimate
+def remEst = issue.estimate
+if (!origEst) return
+return (origEst - remEst) //3600
