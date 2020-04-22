39b87c015c90c766ba2d44a0149792a062d3b7c4
TotallyTot <fcukthepop@gmail.com>
2020-04-22 16:23:38 +0300

    new scripted field for original estimate calculation in issues related to Epic issue

---
+++ src/main/resources/jira/scripted_fields/MaxValOfDateFieldInEpicIssues.groovy
@@ -0,0 +1,18 @@
+package jira.scripted_fields
+
+import com.atlassian.jira.component.ComponentAccessor
+import java.sql.Timestamp
+
+if (issue.issueType.name != "Epic") return
+def epicIssues = ComponentAccessor.issueLinkManager.getOutwardLinks(issue.id)
+        .findAll { it.issueLinkType.name == "Epic-Story Link" }
+        *.destinationObject
+if (epicIssues.empty) return
+def actualEndDate = ComponentAccessor.customFieldManager.customFieldObjects
+        .find { it.name == "Actual End Date" }
+def allEndDateVals = epicIssues
+        .findAll { actualEndDate.getValue(it) != null }
+        .collect { actualEndDate.getValue(it) as Timestamp}
+if (allEndDateVals.empty) return
+Collections.sort(allEndDateVals)
+return allEndDateVals.last()
--- src/main/resources/jira/scripted_fields/MinValOfDateFieldInEpicIssues.groovy
+++ src/main/resources/jira/scripted_fields/MinValOfDateFieldInEpicIssues.groovy
@@ -2,16 +2,17 @@ package jira.scripted_fields
 
 import com.atlassian.jira.component.ComponentAccessor
 import java.sql.Timestamp
-import java.text.SimpleDateFormat
 
 if (issue.issueType.name != "Epic") return
 def epicIssues = ComponentAccessor.issueLinkManager.getOutwardLinks(issue.id)
-        .findAll { it.issueLinkType.name == "Epic-Story Link" }*.destinationObject
+        .findAll { it.issueLinkType.name == "Epic-Story Link" }
+        *.destinationObject
 if (epicIssues.empty) return
 def actualStartDate = ComponentAccessor.customFieldManager.customFieldObjects
         .find { it.name == "Actual Start Date" }
 def allStartDateVals = epicIssues
         .findAll { actualStartDate.getValue(it) != null }
         .collect { actualStartDate.getValue(it) as Timestamp}
+if (allStartDateVals.empty) return
 Collections.sort(allStartDateVals)
-return new SimpleDateFormat("dd/MMM/yy").format(allStartDateVals.first())
+return allStartDateVals.first()
