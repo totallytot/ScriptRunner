0593b965806b59abca7991bebc2c4be14b1ac210
TotallyTot <fcukthepop@gmail.com>
2020-05-03 19:37:18 +0300

    post-functions for Insight app

--- src/main/resources/jira/post_functions/insight/SetInsightObjectFieldBasedOnReporterRetrievedViaIQL.groovy
+++ src/main/resources/jira/post_functions/insight/SetInsightObjectFieldBasedOnReporterRetrievedViaIQL.groovy
@@ -9,7 +9,7 @@ if (issue.issueType.name == "Инцидент") return
 def executionUser = ComponentAccessor.userManager.getUserByName('Admin')
 
 // search in Insight
-// ComponentAccessor.jiraAuthenticationContext.setLoggedInUser(executionUser)
+ComponentAccessor.jiraAuthenticationContext.setLoggedInUser(executionUser)
 def iql = "objectType = Клиент AND \"Код пользователя\" = ${issue.reporter.name}"
 def iqlFacade = ComponentAccessor.getOSGiComponentInstanceOfType(ComponentAccessor.getPluginAccessor()
         .getClassLoader().findClass("com.riadalabs.jira.plugins.insight.channel.external.api.facade.IQLFacade"))
@@ -20,10 +20,10 @@ def objectFacade = ComponentAccessor.getOSGiComponentInstanceOfType(ComponentAcc
         .getClassLoader().findClass("com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectFacade"))
 def companyKey = objectFacade.loadObjectAttributeBean(client[0].id, "Компания").objectAttributeValueBeans[0].value
 def company = objectFacade.loadObjectBean("ITSM-${companyKey}")
-def insightCF = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(10267)
+def insightCF = ComponentAccessor.customFieldManager.getCustomFieldObject(10267)
 
 // update issue w/t notification
 if (!company || !insightCF) return
 def companies = [company]
 issue.setCustomFieldValue(insightCF, companies)
-ComponentAccessor.getIssueManager().updateIssue(executionUser, issue, EventDispatchOption.DO_NOT_DISPATCH, false)
+ComponentAccessor.issueManager.updateIssue(executionUser, issue, EventDispatchOption.DO_NOT_DISPATCH, false)
