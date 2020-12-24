94e8ebda7cfcba1778646da84ea8fdb37eea142a
TotallyTot <fcukthepop@gmail.com>
2020-12-24 19:12:49 +0300

    minor fixes

--- src/main/jira/scripted_fields/LastComment.groovy
+++ src/main/jira/scripted_fields/LastComment.groovy
@@ -1,13 +1,10 @@
 package jira.scripted_fields
-//Script Location: Sript Field
-//Shows last comment for each JIRA issue. Usefull in search for issues.
-//String output was rendered to wiki.
 
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.fields.renderer.IssueRenderContext
 
 def commentManager = ComponentAccessor.commentManager
-def comment = commentManager?.getLastComment(issue)
+def comment = commentManager.getLastComment(issue)
 def lastComment = null
 if (comment) {
     lastComment = comment.body
