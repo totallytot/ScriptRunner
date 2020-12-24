43f08e5e5c4ece46b1d97b7cf735d98c37505127
Totally Tot <fcukthepop@gmail.com>
2020-12-24 19:07:44 +0300

    Create ReopenCounter.groovy

---
+++ src/main/jira/scripted_fields/ReopenCounter.groovy
@@ -0,0 +1,9 @@
+package jira.scripted_fields
+
+import com.atlassian.jira.component.ComponentAccessor
+
+def changeHistoryManager = ComponentAccessor.changeHistoryManager
+def statusChangeItemBean = changeHistoryManager.getChangeItemsForField(issue, "status")
+int reopenedCount = statusChangeItemBean.findAll { it.toString == "Reopened" }?.size()
+int undoReopenCount = statusChangeItemBean.findAll { it.toString == "Undo Reopen" }?.size()
+return reopenedCount - undoReopenCount
