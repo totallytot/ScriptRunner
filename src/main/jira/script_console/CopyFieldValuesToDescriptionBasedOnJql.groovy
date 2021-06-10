8f2fae161efbe23af6c7d62b6a35dd835ea57eb1
TotallyTot <fcukthepop@gmail.com>
2021-06-10 13:17:03 +0300

    minor fix

--- src/main/jira/script_console/CopyFieldValuesToDescriptionBasedOnJql.groovy
+++ src/main/jira/script_console/CopyFieldValuesToDescriptionBasedOnJql.groovy
@@ -11,28 +11,38 @@ import com.atlassian.jira.web.bean.PagerFilter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-final String JQL = 'issue in ("TEST-3123", "TEST-3114")'
-final List<String> CUSTOM_FIELD_NAMES = ["Text 00", "Text 01", "Text 02"]
+final String JQL = "issue in (CFSU-769)"
+final List<String> CUSTOM_FIELD_NAMES = ["Database", "Reproducible"]
 
 def executionUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
 def searchResult = getIssuesFromJql(executionUser, JQL)
 
 if (searchResult.empty) return
 def format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-def separator = """
+def mainSeparator = """
 ___________________
 The text below was added from archived custom fields on ${LocalDateTime.now().format(format)}
 ___________________
+"""
+def fieldSeparator = """
+Currently - ___________________
+Reason For Chosen Priority: Technical documents are client facing.
+Steps To Reproduce : Run a month end close in the system.
 """
 searchResult.each { Issue issue ->
     def fieldValMapping = CUSTOM_FIELD_NAMES.collectEntries { [it, getCustomFieldValue(it, issue)] }
     def descVal = StringBuilder.newInstance()
     descVal << issue.description
-    descVal << separator
+    descVal << mainSeparator
     fieldValMapping.each { k, v ->
-        if (v) descVal << k.toString().concat(": ").concat(v as String).concat("\\\\")
+        if (v) {
+            descVal << k.toString().concat(": ").concat(v as String)
+            descVal << fieldSeparator
+        }
     }
-    setDescription(executionUser, issue, descVal as String)
+    def result = setDescription(executionUser, issue, descVal as String)
+    log.warn """Working with ${issue.key}"""
+    log.warn """Update result: ${result}"""
 }
 
 static List<Issue> getIssuesFromJql(ApplicationUser executionUser, String jql) {
