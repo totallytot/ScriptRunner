d8c80378601ad83b143b67af6e8fe06df9c70556
TotallyTot <fcukthepop@gmail.com>
2021-09-27 01:44:51 +0300

    changes in logic

--- src/main/jira_cloud/listeners/set_field_value_based_on/SlaBreachDate.groovy
+++ src/main/jira_cloud/listeners/set_field_value_based_on/SlaBreachDate.groovy
@@ -2,110 +2,97 @@ package jira_cloud.listeners.set_field_value_based_on
 
 import kong.unirest.Unirest
 
-import java.time.DayOfWeek
-import java.time.LocalDate
-
 enum SlaFieldMapping {
-    SLA_3("CR 3d", 3, "customfield_11676", "CR_target_3"),
-    SLA_7("CR 7d", 7, "customfield_11677", "CR_target_7"),
+    SLA_00("CR 3d", "customfield_11676", 10004), // CR - CR_target_3
+    SLA_01("CR 7d", "customfield_11677", 10004), // // CR - CR_target_7
+    SLA_02("LAW 7d", "customfield_11672", 10000), // Law - law_target_7
+    SLA_03("LAW 15d", "customfield_11673", 10000), // Law - law_target_15
+    SLA_04("LAW 25d", "customfield_11674", 10000), // Law - law_target_25
+    SLA_05("Law AR 7d", "customfield_11687", 10002), // Law AR - law_AR_target_7
+    SLA_06("Law AR 15d", "customfield_11688", 10002) // // Law AR - law_AR_target_15
 
     final String slaName
-    final int days
     final String fieldId
-    final String fieldName // just for info
+    final int issueTypeId
 
-    SlaFieldMapping(String slaName, int days, String fieldId, String fieldName) {
+    SlaFieldMapping(String slaName, String fieldId, int issueTypeId) {
         this.slaName = slaName
         this.fieldId = fieldId
-        this.fieldName = fieldName
-        this.days = days
+        this.issueTypeId = issueTypeId
     }
 
     static String getFieldIdBySlaName(String slaName) {
         this.find { it.slaName == slaName }?.fieldId
     }
 
-    static int getDaysByFieldId(String fieldId) {
-        this.find { it.fieldId == fieldId }?.days
+    static boolean isAllowedIssueType(int issueTypeId) {
+        this.any { it.issueTypeId == issueTypeId }
     }
 }
-
-final String LAW_DATE_FIELD_ID = "customfield_11645" // law_date
-final String LAW_DUE_DATE_FIELD_ID = "customfield_11646" // law_due_date
-final String TRIGGER_ISSUE_TYPE_ID = "10004" // CR
 
 logger.info "SLA BREACH DATE LISTENER START"
 
 logger.info "Working with ${issue.key}"
-def isAllowedIssueType = issue.fields.issuetype.id == TRIGGER_ISSUE_TYPE_ID
-def isLawDateChange = changelog.items.any { Map changeItem ->
-    (changeItem.fieldId as String) in [LAW_DATE_FIELD_ID, LAW_DUE_DATE_FIELD_ID]
-}
-if (!isAllowedIssueType && !isLawDateChange) {
+def isAllowedIssueType = SlaFieldMapping.isAllowedIssueType(issue.fields.issuetype.id as int)
+if (!isAllowedIssueType) {
     logger.info "Condition not passed - listener stopped"
     return
 }
 logger.info "Condition passed"
-def breachTimeValues = generateFieldValuesBasedOnSlaBreachTime(issue.key)
-if (!breachTimeValues) {
+
+def collectActualSlaData = {
+    def slaDataResult = getSlaData(issue.key as String)
+    if (slaDataResult.status != 200) {
+        logger.error "Error during getting SLA breach time - listener stopped: ${slaDataResult}"
+        return
+    }
+    logger.info "slaDataResult: ${slaDataResult.body.values}"
+    return slaDataResult.body.values.findAll {
+        it.name.toString() in SlaFieldMapping.values().collect { it.slaName }
+    }?.findAll { it.ongoingCycle?.breachTime }
+}
+
+// do several attempts as SLA has delays in setting breach time
+int slaRequestCount = 1
+def actualSLAs = []
+while(actualSLAs.empty) {
+    logger.info "Request SLA attempt: ${slaRequestCount}"
+    actualSLAs = collectActualSlaData()
+    logger.info "SLA exists: ${!actualSLAs.empty}"
+    slaRequestCount++
+    if (slaRequestCount > 4) {
+        logger.info "Sla request count breached"
+        break
+    }
+    sleep(3000)
+}
+
+if (actualSLAs.empty) {
+    logger.info "No actual SLAs in mapping - listener stopped"
+    return
+}
+
+def fieldValuesMapping = actualSLAs.collectEntries {
+    def slaBreachTime = it.ongoingCycle.breachTime.jira
+    def field = SlaFieldMapping.getFieldIdBySlaName(it.name.toString())
+    if (field && slaBreachTime) return [field, slaBreachTime]
+}
+
+if (fieldValuesMapping.keySet().empty) {
     logger.error "Error during values generation from SLA breach time - listener stopped"
     return
 }
-logger.info "Field values based on SLA breach time: ${breachTimeValues.toString()}"
-def isNotFull = breachTimeValues.values().any { it == null }
-logger.info "Fully Generated: ${!isNotFull}"
-
-def lawDate = issue.fields[LAW_DATE_FIELD_ID]
-def lawDueDate = issue.fields[LAW_DUE_DATE_FIELD_ID]
-
-def selectedDate = null
-if (!lawDueDate && lawDate && isNotFull) {
-    logger.info "Generating values based on law_date"
-    selectedDate = LocalDate.parse(lawDate as String)
-} else if (lawDueDate && isNotFull) {
-    logger.info "Generating values based on law_due_date"
-    selectedDate = LocalDate.parse(lawDueDate as String)
-}
-logger.info "selectedDate: ${selectedDate}"
-
-def fieldValuesMapping = [:]
-if (selectedDate) fieldValuesMapping = breachTimeValues.collectEntries { customfieldId, value ->
-        if (!value) {
-            def daysToAdd = SlaFieldMapping.getDaysByFieldId(customfieldId as String)
-            logger.info "daysToAdd: ${daysToAdd}"
-            value = addDaysSkippingWeekends(selectedDate, daysToAdd)
-            logger.info "Generated: ${customfieldId}: ${value}"
-        }
-        [customfieldId, value.toString()]
-    }
-else fieldValuesMapping = breachTimeValues
-logger.info "fieldValuesMapping ${fieldValuesMapping}"
+logger.info "Field values based on SLA breach time: ${fieldValuesMapping.toString()}"
 
 def result = setFields(issue.key as String, fieldValuesMapping)
 if (result.status != 204) logger.error "Error during fields update: ${result.status} ${result.body}"
+
 logger.info "SLA BREACH DATE LISTENER END"
 
-static Map generateFieldValuesBasedOnSlaBreachTime(String issueKey) {
-    def result = Unirest.get("/rest/servicedeskapi/request/${issueKey}/sla")
+static def getSlaData(String issueKey) {
+    Unirest.get("/rest/servicedeskapi/request/${issueKey}/sla")
             .header("Content-Type", "application/json")
             .asObject(Map)
-    if (result.status == 200) {
-        def actualSLAs = result.body.values.findAll { it.name in SlaFieldMapping.values().collect { it.slaName } }
-        actualSLAs.collectEntries {
-            [SlaFieldMapping.getFieldIdBySlaName(it.name as String), it.ongoingCycle?.breachTime?.jira]
-        }
-    } else null
-}
-
-static LocalDate addDaysSkippingWeekends(LocalDate date, int days) {
-    LocalDate result = date
-    int addedDays = 0
-    while (addedDays < days) {
-        result = result.plusDays(1)
-        // Israel
-        if (!(result.dayOfWeek in [DayOfWeek.FRIDAY, DayOfWeek.SATURDAY])) ++addedDays
-    }
-    result
 }
 
 static def setFields(String issueKey, Map fieldValueMapping) {
